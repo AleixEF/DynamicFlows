cbb8c170b1fea86911d16c0310b6141429218c2f
Anubhab Ghosh <findghosh@gmail.com>
2021-08-03 20:23:53 +0200

    Adding comments and slight modifications for present code in data_utils

--- lib/utils/data_utils.py
+++ lib/utils/data_utils.py
@@ -35,31 +35,87 @@ def normalize(xtrain, xtest):
     f_perform_normalize = np.vectorize(partial(norm_min_max, min_=min_tr, max_=max_tr), signature="()->()", otypes=[np.ndarray])
     return f_perform_normalize(xtrain), f_perform_normalize(xtest)
 
-def find_change_loc(x):
-    dx = np.diff(x)
-    # Make a clean vector to delimit phonemes
-    change_locations = np.array([0] + (1 + np.argwhere(dx != 0)).reshape(-1).tolist() + [x.shape[0]])
-    # Make an array of size n_phoneme_in_sentence x 2, containing begining and end of each phoneme in a sentence
-    fmt_interv = np.array([[change_locations[i-1], change_locations[i]]\
-                                 for i in range(1,   change_locations.shape[0]) ])
-    return fmt_interv, x[change_locations[:-1]]
+def find_changed_phoneme_label(label_sequence):
+    """ This function is used to find location / indices where the phoneme label has changed.
+    
+    Underlying assumption -
+
+    Suppose an input sequence has phoneme label:[1,1,1,1,1,1,1,0,0,0,0,0,0,2,2,2,2,2,2,3].
+    
+    This means that the given sequence has four types of phonemes present: 
+    [class '1', class '0', class '2', class '3'], indicated by labels '1', '0', '2' and '3' respectively.
+
+    Our objective is to find the indices at which the labels change.
+    
+    NOTE: We assume that for every sequence, the first and the last time-stamp are going to be indices where 
+    labels change. This is because the labels are actually:
+    ['silence label', class '1', class '0', class '2', class '3', 'silence label']. 
+    
+    We do this by first calculating 
+    a first difference of the label sequence, where the non-zero points will indicate points of change of
+    labels. We manually append indices 0 and len(seq) to the list of 'change locations' based on our 
+    assumption.
+
+    Args:
+        label_sequence ([numpy.array]): Sequence of labels for the given sequence
+
+    Returns:
+        - phoneme_intervals ([numpy.array]): Numpy array containing a list indicating the starting 
+        and beginning of each sequence
+        - changed_labels ([numpy.array]): Numpy array containing the changed labels corresponding to each 
+        phoneme interval
+    """
+
+    # Finds out a first difference of the label sequence to indicate points where the labels are changing
+    first_diff_points = np.diff(label_sequence) 
+
+    # Finding change locations of labels based on our assumption of silence phones before the start and end of the 
+    # sequence, also we add 1 to where every the points change so that it reflects the indices of the original
+    # array and not the first difference of the same.
+    label_change_locations = np.array([0] + (1 + np.argwhere(first_diff_points != 0)).reshape(-1).tolist() + 
+                                [label_sequence.shape[0]])
+    
+    # Make an array of size num_phonemes_in_sentence x 2, containing begining and end of each phoneme in a sentence
+    phoneme_intervals = np.array([[label_change_locations[i-1], label_change_locations[i]]\
+                                 for i in range(1, label_change_locations.shape[0])])
+    
+    # Get corresponding labels for each phoneme_intervals
+    changed_labels = label_sequence[label_change_locations[:-1]]
+
+    return phoneme_intervals, changed_labels
 
 def to_phoneme_level(DATA):
-    n_sequences = len(DATA)
+    """ 
+    This function takes the data comprising of all sequences and provides data partitioned at the
+    phoneme level and return the list of data, labels for each phoneme. 
+    We loop over all the sequences in the dataset DATA is a list of numpy arrays, 
+    the length of DATA is the number of sentences / utterances in the training dataset and 
+    each numpy array in data is of the shape (sentence_length, (feature_dimension + 1)). The "+1"
+    is because the label (a number) for the phone is also present in the first column of the numpy array.
+    """
+    n_sequences = len(DATA) # Get the number of sequences
 
-    seq_train = [0 for _ in range(n_sequences)]
+    # This list is created to store the beginning and end index of each phoneme in a sentence
+    seq_train = [0 for _ in range(n_sequences)] 
+
+    # This list is created to corresponding target for the phoneme in a sentence
     targets_train = [0 for _ in range(n_sequences)]
+
+    # These blank lists are created to store data and labels resepctively for each phone 
     data_tr = []
     labels_tr = []
 
     # For all sentences
+    # NOTE: The format of the data is (label, actual_data)
     for i, x in enumerate(DATA):
-        seq_train[i], targets_train[i] = find_change_loc(x[:, 0])
+        
+        # seq_train[i] contains start-end time indices for each phone in targets_train[i]
+        seq_train[i], targets_train[i] = find_changed_phoneme_label(x[:, 0])
 
-        # Delete label from data
+        # Delete label from data, as labels are present in the first column of the 'data' variable
         x[:, 0] = 0
 
-        # For each phoneme found in the sentence, get the sequence of MFCCs and the label
+        # For each phoneme found in the sentence, get the sequence of MFCCs (features) and the label (number)
         for j in range(seq_train[i].shape[0]):
             data_tr += [x[seq_train[i][j][0]:seq_train[i][j][1]]]
             labels_tr += [targets_train[i][j]]
@@ -68,6 +124,9 @@ def to_phoneme_level(DATA):
     return np.array(data_tr), np.array(labels_tr)
 
 def remove_label(data, labels, phn2int_39):
+    """ This function removes the label '-' from the list of 40 phones (mapped from 61 phones)
+    and retrieves corresponding data and label indices
+    """
     keep_idx = labels != phn2int_39['-']
     data_out = data[keep_idx]
     label_out = labels[keep_idx]
@@ -75,7 +134,10 @@ def remove_label(data, labels, phn2int_39):
     return data_out, label_out
 
 def phn61_to_phn39(label_int_61, int2phn_61=None, data_folder=None, phn2int_39=None):
-    """Group labels based on info found on table 3 of html file."""
+    """
+    Group labels based on info found on table 3 of html file (there is a dictionary 
+    'phoneme_map_61_to_39.json' that was present with the TIMIT dataset in use).
+    """
     with open(os.path.join(data_folder, "phoneme_map_61_to_39.json"), "r") as fp:
         phn61_to_39_map = json.load(fp)
 
@@ -92,7 +154,8 @@ def phn61_to_phn39(label_int_61, int2phn_61=None, data_folder=None, phn2int_39=N
     return np.array(label_int_39), phn2int_39
 
 def flip(d):
-    """In a dictionary, swap keys and values"""
+    """In a dictionary, swap keys and values, this is important while creating the
+    phone to class index dictionaries (and its reverse)"""
     return {v: k for k, v in d.items()}
 
 def read_classmap(folder):
@@ -134,7 +197,7 @@ def pad_data(x, length):
             length : integer, common target length of sequences.
     output: list,  all input sequences zero-padded.
     """
-    #NOTE: This needs to be fixed for the format that we are looking for
+    #TODO: This needs to be fixed for the format that we are looking for
     d = x[0].shape[1]
     return [np.concatenate((xx, np.zeros((length - xx.shape[0] + 1, d)))) for xx in x]
 
@@ -154,7 +217,7 @@ class CustomSequenceDataset(Dataset):
     Args:
         Dataset ([type]): [description]
     """
-    #NOTE: This needs to be fixed for the data format we are talking about
+    #TODO: This needs to be fixed for the data format we are talking about
     def __init__(self, xtrain, lengths, device='cpu'):
         self.data = [torch.FloatTensor(x).to(device) for x in xtrain]
         self.lengths = lengths
@@ -171,7 +234,7 @@ class CustomSequenceDataset(Dataset):
         #return (self.data[idx], self.mask[idx])
 
 def my_collate_fn(batch):
-    #NOTE: Needs to be tested
+    #TODO: Needs to be tested
     batch_expanded_dims = [np.expand_dims(x_sample, axis=1) for x_sample, _ in batch]
     inputs_mbatch_tensor = torch.from_numpy(np.concatenate(batch_expanded_dims, axis=1))
     lengths_ = [x_sample[1] for x_sample in batch]
@@ -179,7 +242,7 @@ def my_collate_fn(batch):
     return (inputs_mbatch_tensor, lengths_mbatch_tensor)
 
 def get_dataloader(dataset, batch_size, tr_indices, val_indices, test_indices=None):
-    #NOTE: Needs to be tested
+    #TODO: Needs to be tested
     custom_loader = DataLoader(dataset,
                             batch_size=batch_size,
                             sampler=torch.utils.data.SubsetRandomSampler(tr_indices),
--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -1,19 +1,36 @@
 import pytest
-from utils.data_utils import *
+import numpy as np
+from context import data_utils 
+#from lib.utils.data_utils import *
 
 def test_norm_min_max():
-    return None
-
-def test_acc_str():
-    return None
+    x = np.array([[1, 3], [2, 2]])
+    min_ = x.min(0)
+    max_ = x.max(0)
+    xn = data_utils.norm_min_max(x, min_=min_, max_=max_)
+    assert ((xn == np.array([[0, 1], [1, 0]])).all())
 
 def test_flip():
     d = {k: v for k, v in zip(list("abcbdefg"), list(range(8)))}
-    assert(d == flip(flip(d)))
+    assert(d == data_utils.flip(data_utils.flip(d)))
 
 def test_norm_prob():
     x = np.array([[1, 2], [4, 5]])
-    xn = norm_prob(x, axis=1)
+    xn = data_utils.norm_prob(x, axis=1)
     assert(np.all(xn == np.array([[1/(1+2), 2/(1+2)], [4/(4+5), 5/(4+5)]])))
-    xn = norm_prob(x, axis=0)
+    xn = data_utils.norm_prob(x, axis=0)
     assert (np.all(xn == np.array([[1 / (1 + 4), 2 / (5 + 2)], [4 / (4 + 1), 5 / (2 + 5)]])))
+
+def test_find_changed_phoneme_label():
+    l1 = np.array([1,1,1,1,1,1,1,0,0,0,0,0,0,2,2,2,2,2,2,3])
+    out, out2 = data_utils.find_changed_phoneme_label(l1)
+    assert out.shape[0] == out2.shape[0]
+    assert((out2 == np.array([1,0,2,3])).all())
+    assert((out == np.array([[0,  7], [7, 13],[13, 19],[19, 20]])).all())
+
+    l2 = np.array([1, 1, 0, 0, 2, 2])
+    out, out2 = data_utils.find_changed_phoneme_label(l2)
+    assert out.shape[0] == out2.shape[0]
+    assert((out2 == np.array([1, 0, 2])).all())
+    assert((out == np.array([[0, 2], [2, 4], [4, 6]])).all())
+
