0d2e339c1bda84e9d09bb343e92de2a2f1305b10
AleixEF <aleix.fontcuberta@gmail.com>
2021-08-25 12:46:15 +0200

    cleaned branch, we will only include hmm classification tests

--- tests/context.py
+++ tests/context.py
@@ -10,9 +10,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
 from lib.src import esn, flows
-
 from lib.utils import data_utils, hmm
+
 
 
