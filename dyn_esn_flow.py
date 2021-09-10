Fix some bugs in the accuracy computation, apparently python's internal sum function can't add bools properly

dyn_esn_flow.py
@@ -117,7 +117,9 @@ class DynESN_gen_model(nn.Module):
                     eval_NLL_loss_batch = -torch.mean(eval_loglike_batch)
                     eval_NLL_loss_epoch_sum += eval_NLL_loss_batch.item()
                 
-                eval_loss = eval_NLL_loss_epoch_sum / len(evalloader)
+                #eval_loss = eval_NLL_loss_epoch_sum / len(evalloader)
+                eval_loss = eval_NLL_loss_epoch_sum / len(evalloader.dataset) 
+
                 print("Test loss for Dyn_ESN_Model {}: {}".format(i+1, eval_loss))
                 print("Test loss for Dyn_ESN_Model {}: {}".format(i+1, eval_loss), file=orig_stdout)
                 dyn_esn_model_llh = torch.cat(dyn_esn_flow_model_LL, dim=0).reshape((1, -1))
@@ -126,14 +128,25 @@ class DynESN_gen_model(nn.Module):
             llh_all_models = torch.cat(llh_per_model_list, dim=0)
             predictions_all_models = torch.argmax(llh_all_models, dim=0) + 1
 
-        true_predictions = torch.empty(size=(len(evalloader.dataset),)).fill_(class_number) # Make a tensor containing the true labels
-        
+        true_predictions = torch.empty(size=(len(evalloader.dataset),)).fill_(class_number).type(torch.LongTensor) # Make a tensor containing the true labels
+        #print(true_predictions)
+        #print(predictions_all_models)
         # Get the classification summary
         print("Getting the classification summary for data corresponding to Class:{}".format(class_number))
-        eval_summary = classification_report(y_true=true_predictions.numpy(), y_pred=predictions_all_models.numpy(), output_dict=True, zero_division=0)
+        
+        if predictions_all_models.is_cuda == True:
+            predictions_all_models = predictions_all_models.cpu()
+        
+        #print(predictions_all_models.dtype, true_predictions.dtype)
+        eval_summary = classification_report(y_true=true_predictions, y_pred=predictions_all_models, output_dict=True, zero_division=0)
         eval_summary["num_sequences"] = len(evalloader.dataset)
-        eval_summary["num_corrects"] = sum(predictions_all_models == true_predictions).item()
-
+        flags = true_predictions == predictions_all_models
+        eval_summary["num_corrects"] = np.count_nonzero(np.array(flags))
+        #eval_summary["accuracy"] = eval_summary["num_corrects"] / eval_summary["num_sequences"]
+        
+        #print(classification_report(y_true=true_predictions, y_pred=predictions_all_models, output_dict=False,  zero_division=0))
+        #print(true_predictions)
+        #print(predictions_all_models)
         sys.stdout = orig_stdout
 
         orig_stdout = sys.stdout
@@ -144,7 +157,7 @@ class DynESN_gen_model(nn.Module):
         print("-"*100, file=orig_stdout)
         print("Accuracy for Class:{}-{} data:{} ({}/{})".format(class_number, mode, eval_summary['accuracy'], eval_summary["num_corrects"], eval_summary["num_sequences"]))
         print("Accuracy for Class:{}-{} data:{} ({}/{})".format(class_number, mode, eval_summary['accuracy'], eval_summary["num_corrects"], eval_summary["num_sequences"]), file=orig_stdout)
-        #print(classification_report(y_true=true_predictions.numpy(), y_pred=model_predictions.numpy(), output_dict=False,  zero_division=0))
+        #print(classification_report(y_true=true_predictions.numpy(), y_pred=predictions_all_models.numpy(), output_dict=False,  zero_division=0))
         print("-"*100)
         print("-"*100, file=orig_stdout)
         with open(os.path.join(datafolder, "class_{}_{}_clsfcn_summary.json".format(class_number, mode)), 'w') as f:

evaluate_dyn_flow.py
@@ -162,7 +162,7 @@ def main():
                                                                                                     noise_type=noise_type, dataset_type=dataset_type, 
                                                                                                     epoch_ckpt_num=epoch_ckpt_number)
         
-        weight_iclass = sum(model_preds_iclass == true_preds_iclass).item() / len(true_preds_iclass)
+        weight_iclass = np.count_nonzero(np.array(model_preds_iclass == true_preds_iclass)) / len(true_preds_iclass)
         total_acc += eval_summary_iclass['accuracy'] * weight_iclass
         sum_of_weights += weight_iclass
 

lib/src/flows.py
@@ -205,7 +205,7 @@ class FlowLayer(nn.Module):
         """
         #print(x_frame.device, h_esn.device, b_mask.device)
         slope, intercept = self.nn(b_mask*x_frame, h_esn)
-        slope = self.rescale_function(slope)  # Performing weight normalization re-scaling
+        slope = self.rescale_function(slope, self.device)  # Performing weight normalization re-scaling
         z_latent = b_mask*x_frame \
             + (1-b_mask) * ((x_frame-intercept) * torch.exp(-slope))
         #log_det = slope @ (b_mask.T - 1)  # final shape (batch_size, 1)
@@ -226,7 +226,7 @@ class FlowLayer(nn.Module):
         """
         #print(z_latent.device, h_esn.device, b_mask.device)
         slope, intercept = self.nn(b_mask*z_latent, h_esn)
-        slope = self.rescale_function(slope)  # Performing weightnorm re-scaling
+        slope = self.rescale_function(slope, self.device)  # Performing weightnorm re-scaling
         x_data_space = b_mask*z_latent \
             + (1-b_mask) * (z_latent*torch.exp(slope) + intercept)
         return x_data_space
@@ -242,6 +242,7 @@ class Rescale(torch.nn.Module):
         super(Rescale, self).__init__()
         self.weight = torch.nn.Parameter(torch.ones(1, frame_dim))
 
-    def forward(self, x):
-        x = self.weight * x
+    def forward(self, x, device='cpu'):
+        #print(self.weight.device, x.device)
+        x = self.weight.to(device) * x
         return x